d31666c7538cea451588719dafadfbb617b6eec3
SoloQTKiller <37258057+SoloQTKiller@users.noreply.github.com>
2023-08-28 02:49:56 +0000

    Códigos aula 7

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,14 +1,18 @@
 import paswordGenerator from 'password-generator';
-import Mail from '../lib/Mail'
+
+import Queue from '../lib/Queue';
 
 export default{
     async store(req, res){
     const {name, email} = req.body;
+
         const user ={
             name,
             email,
             password: paswordGenerator(15, false)
         }
+
+        await Queue.add('RegistrationMail', { user })
 
         return res.json(user);
     }
